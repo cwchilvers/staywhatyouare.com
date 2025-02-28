6ae9cc355996b98185daf6b6f2fafa1bcd918a51
Chandler Chilvers <cwchilvers@gmail.com>
2025-02-28 15:30:53 -0800

    Home page updates.

---
+++ scripts/colorShift.js
@@ -0,0 +1,7 @@
+gsap.to(".color-shift", {
+    filter: "hue-rotate(180deg)",
+    duration: 3,
+    repeat: -1,
+    yoyo: true,
+    ease: "power1.inOut"
+});
---
+++ scripts/ufo.js
@@ -0,0 +1,9 @@
+gsap.to("#ufo", {
+    x: 0.2, 
+    rotation: 15, 
+    duration: 2,
+    repeat: -1,
+    yoyo: true, 
+    ease: "sine.inOut",
+    stagger: 0.1, 
+});
