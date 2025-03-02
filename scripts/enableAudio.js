0bb9fd53a40d857a2a853e9ba4d750cfe3b568a5
Chandler Chilvers <cwchilvers@gmail.com>
2025-03-02 13:11:22 -0800

    Trying to fix broken audio.

--- scripts/enableAudio.js
+++ scripts/enableAudio.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const silent = new Audio('assets/audio/silent.wav');
+    const silent = new Audio('../assets/audio/silent.wav');
 
     document.body.addEventListener('click', () => {
         silent.play();
