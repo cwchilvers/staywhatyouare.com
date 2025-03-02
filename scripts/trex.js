4da979f5079a98cfae274e4b67014f6b7e9c2eb1
Chandler Chilvers <cwchilvers@gmail.com>
2025-03-02 13:33:49 -0800

    Fixes

--- scripts/trex.js
+++ scripts/trex.js
@@ -1,52 +1,44 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const object = document.querySelector("#trex");
+    const object = document.querySelector('#trex');
 
     const sounds = [
-        "/assets/audio/trex/trex-01.wav",
-        "/assets/audio/trex/trex-02.wav",
-        "/assets/audio/trex/trex-03.wav",
-        "/assets/audio/trex/trex-04.wav",
+        '../assets/audio/trex/trex-01.wav',
+        '../assets/audio/trex/trex-02.wav',
+        '../assets/audio/trex/trex-03.wav',
+        '../assets/audio/trex/trex-04.wav'
     ];
 
-    object.addEventListener("click", () => {
+    object.addEventListener('click', () => {
         const randomSound = sounds[Math.floor(Math.random() * sounds.length)];
 
-        const audio = new Audio(randomSound);
-        audio.play()
+        const sound = new Audio(randomSound);
+        sound.play();
 
-        const computedStyle = window.getComputedStyle(object);
-        const transformMatrix = computedStyle.transform;
-
-        let currentX = 0, currentY = 0;
-
-        if (transformMatrix !== "none") {
-            const matrixValues = transformMatrix.split(",");
-            currentX = parseFloat(matrixValues[4]) || 0;
-            currentY = parseFloat(matrixValues[5]) || 0;
-        }
+        const currentX = parseFloat(window.getComputedStyle(object).transform.split(',')[4]) || 0;
+        const currentY = parseFloat(window.getComputedStyle(object).transform.split(',')[5]) || 0;
 
         gsap.to(object, {
-            x: () => gsap.utils.random(-3, 3) + currentX,
-            y: () => gsap.utils.random(-3, 3) + currentY,
+            x: () => gsap.utils.random(-3, 3) + currentX,  
+            y: () => gsap.utils.random(-3, 3) + currentY, 
             repeat: 40,
-            yoyo: true,
+            yoyo: true, 
             duration: 0.02,
             ease: "power1.inOut",
             onRepeat: () => {
                 gsap.to(object, {
-                    x: gsap.utils.random(-3, 3) + currentX,
-                    y: gsap.utils.random(-3, 3) + currentY,
-                    duration: 0.02,
+                    x: gsap.utils.random(-3, 3) + currentX, 
+                    y: gsap.utils.random(-3, 3) + currentY,  
+                    duration: 0.02
                 });
             },
             onComplete: () => {
                 gsap.to(object, {
-                    x: currentX,
-                    y: currentY,
+                    x: currentX,  
+                    y: currentY, 
                     duration: 0.1,
-                    ease: "power1.inOut",
+                    ease: "power1.inOut"
                 });
-            },
+            }
         });
     });
 });
